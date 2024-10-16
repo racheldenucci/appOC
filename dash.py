557400a5c0dfa96899f7c9838f6671aae309c19d
racheldenucci <denuccirachel@gmail.com>
2024-10-16 14:55:20 -0300

    update gauge score

--- dash.py
+++ dash.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import pandas as pd 
 import plotly_express as px
+import plotly.graph_objects as go
 
 st.set_page_config(layout='wide', page_title='Indicadores OC')
 
 st.subheader('Unidade: São Francisco')
+st.header('Score da Unidade')
+fig = go.Figure(go.Indicator(
+    mode='gauge+number',
+    value=95,
+    title={'text':'Score'},
+    gauge={
+        'axis':{'range':[0,100]}
+    }
+))
+
+st.plotly_chart(fig)
+
 st.header('Financeiro')
 st.subheader('Faturamento')
 c1, c2 = st.columns(2)
