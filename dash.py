4e826efee066047ec5aced7986925a8e91e4a944
racheldenucci <denuccirachel@gmail.com>
2024-10-16 15:11:28 -0300

    update financeiro indicadores

--- dash.py
+++ dash.py
@@ -3,11 +3,11 @@ import pandas as pd
 import plotly_express as px
 import plotly.graph_objects as go
 
-st.set_page_config(layout='wide', page_title='Indicadores OC')
+st.set_page_config(page_title='Indicadores OC')
 
 st.write('Unidade: São Francisco')
 
-st.header('Score da Unidade')
+st.header('Visão Geral')
 fig = go.Figure(go.Indicator(
     mode='gauge+number',
     value=95,
@@ -17,15 +17,52 @@ fig = go.Figure(go.Indicator(
         'axis':{'range':[0,100]}
     }
 ))
-cl1, cl2, cl3 = st.columns(3)
+cl1, cl2 = st.columns(2)
 with cl1:
     st.plotly_chart(fig)
 
 
 st.header('Financeiro')
 st.subheader('Faturamento')
+
 c1, c2 = st.columns(2)
+
 with c1:
     st.metric('Atual', value='63.000')
 with c2:
     st.metric('Meta', value='150.000', delta="-87.000")
+
+st.subheader('Venda/Dia')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='5.250')
+with c2:
+    st.metric('Meta', value='7.250', delta='-2.000')
+
+st.subheader('Ticket médio')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='650')
+with c2:
+    st.metric('Meta', value='500', delta='100')
+
+st.subheader('Conversão Valor')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='25%')
+with c2:
+    st.metric('Meta', value='30%', delta='-10%')
+
+st.subheader('Conversão Qauntidade')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='70%')
+with c2:
+    st.metric('Meta', value='50%', delta='20%')
+
+st.subheader('Projeção de venda')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='126.000')
+with c2:
+    st.metric('Meta', value='150.000', delta='-24.000')
