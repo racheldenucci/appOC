819af1fd2bb49754e97dd434d07415dca9c8f136
racheldenucci <denuccirachel@gmail.com>
2024-10-16 14:47:21 -0300

    update

--- dash.py
+++ dash.py
@@ -2,4 +2,13 @@ import streamlit as st
 import pandas as pd 
 import plotly_express as px
 
-st.write('hello, world')
+st.set_page_config(layout='wide', page_title='Indicadores OC')
+
+st.subheader('Unidade: São Francisco')
+st.header('Financeiro')
+st.subheader('Faturamento')
+c1, c2 = st.columns(2)
+with c1:
+    st.metric('Atual', value='63.000')
+with c2:
+    st.metric('Meta', value='150.000', delta="-87.000")
